0d2dc3dfeb96e5ccb9563b8319091c69acf4a9fe
Pablo Venino <pablo.venino@hotmail.com>
2025-03-22 15:54:08 -0300

    feat: Adding DI

--- backend/OrderingSystem.Infra/InfraModule.cs
+++ backend/OrderingSystem.Infra/InfraModule.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OrderingSystem.Application.Interfaces;
 using OrderingSystem.Infra.Data;
+using OrderingSystem.Infra.Repositories;
 
 namespace OrderingSystem.Infra;
 
@@ -15,6 +17,9 @@ public static class InfraModule
     services.AddDbContext<AppDbContext>(opt =>
       opt.UseNpgsql(config.GetConnectionString("DefaultConnection"))
     );
+
+    services.AddScoped(typeof(IEntityRepository<>), typeof(EntityRepository<>));
+    services.AddScoped<IOrderRepository, OrderRepository>();
 
     return services;
   }
